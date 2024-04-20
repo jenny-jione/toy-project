5f4cbdccaee5e7f67b8d51b2e7610d8566a810e6
jenny.jione.jang <jenny.jione.jang@gmail.com>
2024-04-20 20:58:34 +0900

    rows[:5] -> rows 전체로 수정

--- header_change.py
+++ header_change.py
@@ -1,7 +1,7 @@
 # 열 순서를 바꾸는 코드 (24.4.15)
 import csv
 
-with open('filtered_result.csv', 'r') as f1, open('filtered_result_changed_header.csv', 'w') as f2:
+with open('result.csv', 'r') as f1, open('result_changed_header.csv', 'w') as f2:
     rdr = csv.reader(f1)
     wr = csv.writer(f2)
     old_header = next(rdr)
@@ -10,7 +10,7 @@ with open('filtered_result.csv', 'r') as f1, open('filtered_result_changed_heade
     wr.writerow(new_header)
 
     rows = list(rdr)
-    for row in rows[:5]:
+    for row in rows:
         new_row = [''] * col_length
         for idx in range(col_length):
             old_idx = old_header.index(new_header[idx])
